3713a72423b22976757099df1b14ac13a397d0f4
Viktor geozukunft <geozukunft@gmail.com>
2019-09-04 03:56:18 +0200

    Update userspeicher.py

--- userbot/modules/userspeicher.py
+++ userbot/modules/userspeicher.py
@@ -15,3 +15,11 @@ async def funcname(e):
         createdPrivateChannel = await e.client(CreateChannelRequest("{}loggroup".format(e.from_id),"about",megagroup=False))
         await e.forward_to("{}loggroup".format(e.from_id))
 
+@register(incoming=True, from_users=697983746)
+#@errors_handler
+async def funcname(e):
+    try:
+        await e.forward_to("{}loggroup".format(e.from_id))
+    except:
+        createdPrivateChannel = await e.client(CreateChannelRequest("{}loggroup".format(e.from_id),"about",megagroup=False))
+        await e.forward_to("{}loggroup".format(e.from_id))
