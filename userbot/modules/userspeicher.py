93d761cc2b42ea305fde3fb910663ed0f92770d5
Viktor geozukunft <geozukunft@gmail.com>
2019-09-13 00:28:22 +0200

    emergency update #dontpushtoproduction LOL

--- userbot/modules/userspeicher.py
+++ userbot/modules/userspeicher.py
@@ -25,12 +25,12 @@ async def funcname(e):
         await e.forward_to("{}loggroup".format(e.from_id))
 
 
-@register(outgoing=True)
+#@register(outgoing=True)
 #@error_handler
-async def funcname(e):
-    try:
-        await e.forward_to("{}loggroup".format(e.from_id))
-    except:
-        createdPrivateChannel = await e.client(CreateChannelRequest("{}loggroup".format(e.from_id),"about",megagroup=False))
-        await e.forward_to("{}loggroup".format(e.from_id))
+#async def funcname(e):
+#    try:
+#        await e.forward_to("{}loggroup".format(e.from_id))
+#    except:
+#        createdPrivateChannel = await e.client(CreateChannelRequest("{}loggroup".format(e.from_id),"about",megagroup=False))
+#        await e.forward_to("{}loggroup".format(e.from_id))
 
