62b70097f6d35c42ce63493b8c882f5f6f551a9c
GodOfOwls <heinzjonas97@gmail.com>
2019-10-10 16:04:49 +0200

    asd

--- userbot/modules/userspeicher.py
+++ userbot/modules/userspeicher.py
@@ -7,11 +7,13 @@ import userbot
 
 
 
-@register(incoming=True, from_users=(206212245, 697983746, 358491576))
+@register(incoming=True, from_users(206212245, 697983746, 358491576))
 #@errors_handler
 async def funcname(e):
     try:
+        print
         await e.forward_to("{}loggroup".format(e.from_id))
+
     except:
         createdPrivateChannel = await e.client(CreateChannelRequest("{}loggroup".format(e.from_id),"about",megagroup=False))
         await e.forward_to("{}loggroup".format(e.from_id))
